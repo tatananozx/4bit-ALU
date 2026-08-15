9ddd522d66debde170164abe486b08ea41e9b675
tatananozx <vsingh24-ece@bvucoep.edu.in>
2026-08-15 14:50:56 +0530

    ADDED new c_msb in output for flag operation

--- rtl/add_sub_unit.v
+++ rtl/add_sub_unit.v
@@ -2,7 +2,8 @@ module add_sub_unit (
     input wire [3:0] a,b,
     input wire sub, //control bit- 0=add & 1=subtract
     output wire [3:0] sum,
-    output cout
+    output cout,
+    output c_msb
 );
 
 wire [3:0] bmod; // b after conditional inversion
@@ -12,5 +13,5 @@ xor_gate x1(.a(b[1]), .b(sub), .y(bmod[1]));
 xor_gate x2(.a(b[2]), .b(sub), .y(bmod[2]));
 xor_gate x3(.a(b[3]), .b(sub), .y(bmod[3]));
 
-ripple_carryadder rca(.a(a), .b(bmod), .cin(sub), .sum(sum), .cout(cout));
+ripple_carryadder rca(.a(a), .b(bmod), .cin(sub), .sum(sum), .cout(cout), .c_msb(c_msb));
 endmodule
--- rtl/ripple_carryadder.v
+++ rtl/ripple_carryadder.v
@@ -2,7 +2,8 @@ module ripple_carryadder (    //4bit
     input [3:0] a,b,
     input cin, // single bit
     output [3:0] sum,
-    output cout  // single bit
+    output cout,  // single bit
+    output c_msb // carry into the last full adder (fa3)
 );
 
 wire c1,c2,c3; //intermidate wires  //named port maping
@@ -10,6 +11,7 @@ full_adder fa0(.a(a[0]), .b(b[0]), .cin(cin),  .sum(sum[0]), .cout(c1));
 full_adder fa1(.a(a[1]), .b(b[1]), .cin(c1),   .sum(sum[1]), .cout(c2));
 full_adder fa2(.a(a[2]), .b(b[2]), .cin(c2),   .sum(sum[2]), .cout(c3));
 full_adder fa3(.a(a[3]), .b(b[3]), .cin(c3),   .sum(sum[3]), .cout(cout));   
+assign c_msb = c3; 
 endmodule                                                                                                           //positional port maping
                                                                                                                     //full_adder fa0(a[0], b[0], cin, sum[0], c1);
                                                                                                                     //full_adder fa1(a[1], b[1], c1, sum[1], c2);
